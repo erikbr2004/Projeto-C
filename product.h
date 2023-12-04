097f592a15d8cd13f459d2424c234e5a9e52335a
Erik B. de Castro <erikbr200@gmail.com>
2023-12-04 04:41:46 -0300

    comentei blablabla

--- product.h
+++ product.h
@@ -15,5 +15,5 @@ typedef struct
 
 void createProd(FILE* file, char* nameProd, float priceProd, int maxQuantityProd, int productQuantity);	// Function to create a new product entry in a file
 void deleteProd(FILE* file, int idProd);	// Function to delete a product from the file based on the specified ID
-void checkProd(FILE* file, int idProd);		
+void checkProd(FILE* file, int idProd);		//
 void listOfProd(FILE* file);	// Function to list all products from a file
